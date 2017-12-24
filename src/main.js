98e76cb6c14cb97d9612ae2e4d8a5bc5351e4274
lan <17713476019@163.com>
2017-12-24 13:03:32 +0800

    rename

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import store from './vuex/vuex' 
+import store from './store/vuex.js' 
 
 Vue.config.productionTip = false;
  
